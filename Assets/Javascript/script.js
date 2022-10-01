0503eaa4d4473958af7e29a7bbf9800e16c3e7eb
huntken26 <huntken26@gmail.com>
2022-09-30 21:49:40 -0500

    Added links to the html, formatted the documents, ready to deploy

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -2,56 +2,56 @@
 var timeNow = moment();
 $("#currentDay").text(timeNow.format("LLLL"));
 
+
 // storage() sets an onclick event for each save button, also sets/gets local storage for the time block inputs.
 function storage() {
-    $(".saveBtn").on("click", function () {
-      let inputs = $(this).siblings(".input").val();
-      let time = $(this).parent().attr("id");
-      localStorage.setItem(time, inputs);
-    });
-  
-    $("#am9 .input").val(localStorage.getItem("am9"));
-  
-    $("#am10 .input").val(localStorage.getItem("am10"));
-  
-    $("#am11 .input").val(localStorage.getItem("am11"));
-  
-    $("#noon .input").val(localStorage.getItem("noon"));
-  
-    $("#pm1 .input").val(localStorage.getItem("pm1"));
-  
-    $("#pm2 .input").val(localStorage.getItem("pm2"));
-  
-    $("#pm3 .input").val(localStorage.getItem("pm3"));
-  
-    $("#pm4 .input").val(localStorage.getItem("pm4"));
-  
-    $("#pm5 .input").val(localStorage.getItem("pm5"));
-  }
+  $(".saveBtn").on("click", function () {
+    let inputs = $(this).siblings(".input").val();
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, inputs);
+  });
 
-  //function uses jQuery's each to loop the time blocks; also sets the conditions for the rows to be color coded.
+  $("#am9 .input").val(localStorage.getItem("am9"));
+
+  $("#am10 .input").val(localStorage.getItem("am10"));
+
+  $("#am11 .input").val(localStorage.getItem("am11"));
+
+  $("#noon .input").val(localStorage.getItem("noon"));
+
+  $("#pm1 .input").val(localStorage.getItem("pm1"));
+
+  $("#pm2 .input").val(localStorage.getItem("pm2"));
+
+  $("#pm3 .input").val(localStorage.getItem("pm3"));
+
+  $("#pm4 .input").val(localStorage.getItem("pm4"));
+
+  $("#pm5 .input").val(localStorage.getItem("pm5"));
+}
+
+
+//function uses jQuery's each to loop the time blocks; also sets the conditions for the rows to be color coded.
 function calendar() {
-    $(".row").each(function () {
-      let otherColors = parseFloat($(this).attr("id").split("hour")[1]);
-      let red = moment().hour();
-  
-      if (otherColors < red) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-      } else if (otherColors === red) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-      } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
-    });
-  }
+  $(".row").each(function () {
+    let otherColors = parseFloat($(this).attr("id").split("hour")[1]);
+    let red = moment().hour();
 
-  storage();
+    if (otherColors < red) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else if (otherColors === red) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+storage();
 calendar();
-
-
